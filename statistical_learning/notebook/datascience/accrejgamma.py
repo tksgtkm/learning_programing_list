4e152613f8bb0a02eb71521680ca77f50058b3ba
tksgtkm <h9cd342fd7jgx36@gmail.com>
2024-12-15 09:59:51 +0900

    ノートブック用のディレクトリ作成

---
+++ statistical_learning/notebook/datascience/accrejgamma.py
@@ -0,0 +1,15 @@
+from math import exp, gamma, log
+from numpy.random import rand
+
+alpha = 1.3
+lam = 5.6
+f = lambda x: lam**alpha * x**(alpha - 1) * exp(-lam*x)/gamma(alpha)
+g = lambda x: 4*exp(-4*x)
+C = 1.2
+
+found = False
+while not found:
+    x = -log(rand())/4
+    if C*g(x)*rand() <= f(x):
+        found = True
+print(x)
