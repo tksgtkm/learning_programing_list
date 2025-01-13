768736f8371122506e75b96d8b6aead361a144bf
tksgtkm <h9cd342fd7jgx36@gmail.com>
2025-01-13 21:49:53 +0900

    intro duckdb

--- infra/create_buzz2db.py
+++
@@ -1,7 +0,0 @@
-import duckdb
-
-cursor = duckdb.connect('buzz2.db')
-# print(cursor.execute("SELECT 42").fetchall())
-
-cursor.execute("CREATE TABLE t2(i INTEGER, j INTEGER)")
-cursor.execute("INSERT INTO t2 VALUES (1, 2), (2, 3), (3, 4)")
